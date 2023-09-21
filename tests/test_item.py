bed68445cc07d51d856e9a24f79f854ccd5e5698
olga voronets <voronets80@gmail.com>
2023-09-21 13:53:30 +0300

    Реализованы методы для класса Item, получающие общую стоимость и применяющие скидку для конкретного товара. Присоздании экземпляоа класса он добавляется в общий список all

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,19 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+
+from src.item import Item
+
+item1 = Item("Смартфон", 10000, 20)
+item2 = Item("Ноутбук", 20000, 5)
+
+
+def test_calculate_total_price():
+    assert item1.calculate_total_price() == 200000
+    assert item2.calculate_total_price() == 100000
+
+
+def test_pay_rate():
+    Item.pay_rate = 0.8
+    item1.apply_discount()
+    assert item1.price == 8000.0
+    assert item2.price == 20000
