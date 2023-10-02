f12f8a9f1930f88812e3c156881eb0214225a3cf
olga voronets <voronets80@gmail.com>
2023-10-02 15:13:06 +0300

    создан новый класс `Phone`, реализована возможность сложения экземпляров класса `Phone` и `Item` по количеству товара в магазине, реализована проверка принадлежности складываемых экземпляров классам `Phone` или `Item` Добавлен `tests/test_phone.py`, Добавлен test_add_function в файл `tests/test_item.py`

---
+++ src/phone.py
@@ -0,0 +1,23 @@
+from src.item import Item
+
+
+class Phone(Item):
+
+    def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
+        super().__init__(name, price, quantity)
+        self.__number_of_sim = number_of_sim
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+
+    @property
+    def number_of_sim(self):
+        """Геттер для кол-ва симкарт"""
+        return self.__number_of_sim
+    @number_of_sim.setter
+    def number_of_sim(self, new_number):
+        """сеттер для кол-ва симкарт"""
+        if new_number > 0 and isinstance(new_number, int):
+            self.__number_of_sim = new_number
+        raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля")
+
